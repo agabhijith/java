cf4ec081e45b8a865d6fb47bf40f5a63f66b4618
agabhijith <agabhijith2028@cs.sjcetpalai.ac.in>
2025-07-16 10:04:18 +0530

    Update Input.java

--- Input.java
+++ Input.java
@@ -1,5 +1,13 @@
-package exp1;
+package exp1; 
+import java.util.Scanner; 
 
 public class Input {
-
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("enter a number"); 
+        int number = sc.nextInt(); 
+        System.out.println("the number: " + number); 
+    }
 }
+
+
